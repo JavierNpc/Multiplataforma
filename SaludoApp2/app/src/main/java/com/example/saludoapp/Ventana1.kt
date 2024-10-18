0a29e5e49e7629ee78ccba0ac62f8fcbcb9cf865
JavierNpc <javiermacedacastro@gmail.com>
2024-10-18 13:10:00 +0200

    modificado

--- SaludoApp2/app/src/main/java/com/example/saludoapp/Ventana1.kt
+++ SaludoApp2/app/src/main/java/com/example/saludoapp/Ventana1.kt
@@ -1,6 +1,9 @@
 package com.example.saludoapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +14,25 @@ class Ventana1 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_ventana1)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.ventana1)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val botonVentana1: Button = findViewById(R.id.botonvent)
+
+        botonVentana1.setOnClickListener{
+            val intentMain = Intent(this, MainActivity::class.java)
+
+            startActivity(intentMain)
+        }
+
+        val tvGreeting:TextView = findViewById(R.id.tvGreeting)
+        val name:String =
+            intent.extras?.getString("EXTRA_NAME").orEmpty()
+        tvGreeting.text = "Hola $name"
+
+
     }
 }
