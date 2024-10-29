1e406e88018c36bb46642c1bb7cf2ebb55da1566
JavierNpc <javiermacedacastro@gmail.com>
2024-10-29 14:04:09 +0100

    mod 29 /10

--- AppICM/app/src/main/java/com/example/appicm/AppIMC.kt
+++ AppICM/app/src/main/java/com/example/appicm/AppIMC.kt
@@ -1,22 +1,48 @@
 package com.example.appicm
 
+import android.icu.text.DecimalFormat
 import android.os.Bundle
+import android.support.v4.os.IResultReceiver._Parcel
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.slider.RangeSlider
 
 class AppIMC : AppCompatActivity() {
+    private var isMaleSelected : Boolean = true
     private lateinit var viewMale: CardView
     private lateinit var viewFemale: CardView
-    private var isMaleSelected : Boolean = true
+    private lateinit var rsHeight: RangeSlider
+    private lateinit var tvHeight: TextView
+    private lateinit var tvPeso : TextView
+    private lateinit var tvEdad : TextView
+    private lateinit var btnPesoMas : FloatingActionButton
+    private lateinit var btnPesoMenos : FloatingActionButton
+    private lateinit var btnEdadMas : FloatingActionButton
+    private lateinit var btnEdadMenos : FloatingActionButton
+    private lateinit var btnCalcular : Button
 
 
     private fun initComponents() {
         viewMale = findViewById(R.id.cardMale)
         viewFemale = findViewById(R.id.cardFemale)
+        rsHeight  = findViewById(R.id.rsHeight)
+        tvHeight = findViewById(R.id.tvHeight)
+        tvEdad = findViewById(R.id.tvEdad)
+        tvPeso = findViewById(R.id.tvPeso)
+        btnEdadMas = findViewById(R.id.btnEdadMas)
+        btnEdadMenos = findViewById(R.id.btnEdadMenos)
+        btnPesoMas = findViewById(R.id.btnPesoMas)
+        btnPesoMenos = findViewById(R.id.btnPesoMenos)
+        btnCalcular = findViewById(R.id.btnCalcular)
+
     }
 
 
@@ -29,6 +55,33 @@ class AppIMC : AppCompatActivity() {
             isMaleSelected=true
             setGenderColor();
         }
+        rsHeight.addOnChangeListener { _, value, _ ->
+            //tvHeight.text = value.toString()
+            tvHeight.text = DecimalFormat("#.##").format(value) + " cm"
+        }
+
+        btnEdadMas.setOnClickListener{
+            tvEdad.text = DecimalFormat("##.#").format(setEdad() + 1)
+        }
+        btnEdadMenos.setOnClickListener{
+            tvEdad.text = DecimalFormat("##.#").format(setEdad() - 1)
+        }
+
+
+        btnPesoMas.setOnClickListener{
+            tvPeso.text = DecimalFormat("##.#").format(setPeso() + 1)
+        }
+
+        btnPesoMenos.setOnClickListener{
+            tvPeso.text = DecimalFormat("##.#").format(setPeso() - 1)
+        }
+
+        btnCalcular.setOnClickListener{
+
+        }
+
+
+
     }
 
     private fun setGenderColor() {
@@ -48,6 +101,17 @@ class AppIMC : AppCompatActivity() {
 
     private fun initUI() {
         setGenderColor()
+        setEdad()
+        setPeso()
+    }
+
+    private fun setPeso() : Int  {
+        return Integer.parseInt(tvPeso.text.toString())
+
+    }
+
+    private fun setEdad() : Int {
+        return Integer.parseInt(tvEdad.text.toString())
     }
 
 
