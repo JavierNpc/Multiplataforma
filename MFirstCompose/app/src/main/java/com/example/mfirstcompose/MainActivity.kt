d0aacec810355baed274cb2b4ec7b92ccd0c46e5
JavierNpc <javiermacedacastro@gmail.com>
2024-11-12 14:08:42 +0100

    modificado 12/11

--- MFirstCompose/app/src/main/java/com/example/mfirstcompose/MainActivity.kt
+++ MFirstCompose/app/src/main/java/com/example/mfirstcompose/MainActivity.kt
@@ -1,17 +1,38 @@
 package com.example.mfirstcompose
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.absolutePadding
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.mfirstcompose.ui.theme.MFirstComposeTheme
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,11 +40,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MFirstComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
+
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
+
                 }
             }
         }
@@ -38,10 +61,41 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    MFirstComposeTheme {
-        Greeting("Android")
+fun MyBox() {
+    Box(modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.CenterEnd) {
+        Box(modifier = Modifier
+            .width(200.dp)
+            .height(200.dp)
+            .background(Color.Cyan)
+            .verticalScroll(rememberScrollState()),
+            contentAlignment = Alignment.Center)
+        {
+            Text(text = "Esto es un EJEMPLO",
+                 fontSize = 70.sp )
+        }
     }
 }
+
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun MyPreview() {
+    MFirstComposeTheme {
+        MyBox()
+        //MyColumn()
+        //MyRow()
+        //MyComplexLayout()
+    }
+}
+
+
+
+
+
+
+
+
