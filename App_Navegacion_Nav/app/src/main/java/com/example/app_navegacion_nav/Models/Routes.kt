6a091e760753b8e6685ed08510758b0a895875fb
JavierNpc <javiermacedacastro@gmail.com>
2025-01-08 12:20:29 +0100

    8/1

--- App_Navegacion_Nav/app/src/main/java/com/example/app_navegacion_nav/Models/Routes.kt
+++ App_Navegacion_Nav/app/src/main/java/com/example/app_navegacion_nav/Models/Routes.kt
@@ -2,7 +2,9 @@ package com.example.app_navegacion_nav.Models
 
 sealed class Routes(val route:String) {
     object Ventana1:Routes("Ventana1")
-    object Ventana2:Routes("Ventana2")
+    object Ventana2:Routes("Ventana2/{name}"){
+        fun createRoute(name:String) = "String&name"
+    }
     object Ventana3:Routes("Ventana3")
 
 }
